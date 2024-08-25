ac874c81a4d3f52c4d83e98c09a21e3c67c6725e
krishna66-tech <krishnasai760@gmail.com>
2024-08-25 11:04:30 -0400

    Update mycomponent.js

--- components/mycomponent.js
+++ components/mycomponent.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import './MyComponent.css'; // Optional for styling
+
+const MyComponent = () => {
+  return (
+    <div className="my-component">
+      <h2>My Component</h2>
+      <p>This is my new component!</p>
+    </div>
+  );
+};
+
+export default MyComponent;
 
